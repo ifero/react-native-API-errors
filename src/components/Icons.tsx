b7df0696e802d27eca4240ede21958e8b976557f
Andrea Pacino <andrea.pacino87@gmail.com>
2023-10-10 16:54:47 +0200

    refactor: clean up code

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -23,37 +23,6 @@ export const NearFormLogo = forwardRef<Svg, IconProps>(
   ),
 );
 
-export const AlertIcon = forwardRef<Svg, IconProps>(
-  ({ size = DEFAULT_SIZE, color = theme.colorWhite }, ref) => (
-    <Svg width={size} height={size} ref={ref}>
-      <G
-        stroke="none"
-        strokeWidth={0}
-        strokeDasharray="none"
-        strokeLinecap="butt"
-        strokeLinejoin="miter"
-        strokeMiterlimit={10}
-        fill={'none'}
-        fillRule="nonzero"
-        opacity={1}
-      >
-        <Path
-          d="M85.429 85.078H4.571a5.014 5.014 0 0 1-4.387-2.533 5.011 5.011 0 0 1 0-5.065L40.613 7.455A5.015 5.015 0 0 1 45 4.922c1.832 0 3.471.947 4.386 2.533L89.815 77.48a5.014 5.014 0 0 1 .001 5.065 5.013 5.013 0 0 1-4.387 2.533zM45 7.922c-.747 0-1.416.386-1.79 1.033L2.782 78.979a2.045 2.045 0 0 0 0 2.065 2.045 2.045 0 0 0 1.789 1.033h80.858c.747 0 1.416-.387 1.789-1.033s.373-1.419 0-2.065L46.789 8.955A2.044 2.044 0 0 0 45 7.922zm0 67.403c-4.105 0-7.446-3.34-7.446-7.445s3.34-7.445 7.446-7.445 7.445 3.34 7.445 7.445-3.339 7.445-7.445 7.445zm0-11.89c-2.451 0-4.446 1.994-4.446 4.445s1.995 4.445 4.446 4.445 4.445-1.994 4.445-4.445-1.994-4.445-4.445-4.445zm0-6.289a6.89 6.89 0 0 1-6.882-6.882V34.121A6.89 6.89 0 0 1 45 27.239a6.89 6.89 0 0 1 6.881 6.882v16.144A6.888 6.888 0 0 1 45 57.146zm0-26.907a3.886 3.886 0 0 0-3.882 3.882v16.144A3.886 3.886 0 0 0 45 54.147a3.886 3.886 0 0 0 3.881-3.882V34.121A3.886 3.886 0 0 0 45 30.239z"
-          stroke="none"
-          strokeWidth={1}
-          strokeDasharray="none"
-          strokeLinecap="butt"
-          strokeLinejoin="miter"
-          strokeMiterlimit={10}
-          fill={color}
-          fillRule="nonzero"
-          opacity={1}
-          transform="matrix(2.81 0 0 2.81 1.407 1.407)"
-        />
-      </G>
-    </Svg>
-  ),
-);
 export const Chevron: FC<
   IconProps & { direction: 'up' | 'right' | 'down' | 'left' } & {
     strokeWidth?: number;
